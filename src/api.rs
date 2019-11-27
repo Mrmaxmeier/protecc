521fdca64040de923d0cb0d06c2b72d2bd0751f3
CherryWorm <nico.gruendel@web.de>
2019-11-27 21:48:35 +0100

    lügen

---
+++ src/api.rs
@@ -0,0 +1,41 @@
+use tonic::{Request, Response, Status, Streaming};
+use futures::Stream;
+
+pub mod api {
+    tonic::include_proto!("api");
+}
+
+pub use api::{
+    server,
+    server::{ToolApi, ToolApiServer},
+    RegisterStreamTaggerRequest,
+    RegisterStreamMapperRequest,
+    RegisterStreamReducerRequest,
+    RegisterPacketTaggerRequest
+};
+use std::pin::Pin;
+
+pub struct ToolApiImpl {}
+
+#[tonic::async_trait]
+impl ToolApi for ToolApiImpl {
+    type RegisterStreamTaggerStream = Pin<Box<dyn Stream<Item = Result<api::Stream, Status>> + Send + Sync + 'static>>;
+    async fn register_stream_tagger(&self, request: Request<Streaming<RegisterStreamTaggerRequest>>) -> Result<Response<Self::RegisterStreamTaggerStream>, Status> {
+        unimplemented!()
+    }
+
+    type RegisterStreamMapperStream = Pin<Box<dyn Stream<Item = Result<api::Stream, Status>> + Send + Sync + 'static>>;
+    async fn register_stream_mapper(&self, request: Request<Streaming<RegisterStreamMapperRequest>>) -> Result<Response<Self::RegisterStreamMapperStream>, Status> {
+        unimplemented!()
+    }
+
+    type RegisterStreamReducerStream = Pin<Box<dyn Stream<Item = Result<api::RegisterStreamReducerResponse, Status>> + Send + Sync + 'static>>;
+    async fn register_stream_reducer(&self, request: Request<Streaming<RegisterStreamReducerRequest>>) -> Result<Response<Self::RegisterStreamReducerStream>, Status> {
+        unimplemented!()
+    }
+
+    type RegisterPacketTaggerStream = Pin<Box<dyn Stream<Item = Result<api::UdpPacket, Status>> + Send + Sync + 'static>>;
+    async fn register_packet_tagger(&self, request: Request<Streaming<RegisterPacketTaggerRequest>>) -> Result<Response<Self::RegisterPacketTaggerStream>, Status> {
+        unimplemented!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use tonic::transport::Server;
+use std::sync::Arc;
+use crate::api::ToolApiImpl;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:10000".parse().unwrap();
+
+    Server::builder()
+        .add_service(api::server::ToolApiServer::new(ToolApiImpl {}))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
