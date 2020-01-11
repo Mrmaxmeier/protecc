492f89988801ff8aed34d3196ab2f1860fa9c989
Mrmaxmeier <Mrmaxmeier@gmail.com>
2020-01-11 21:56:11 +0100

    window: implement other indices

--- backend/src/window.rs
+++ backend/src/window.rs
@@ -26,12 +26,14 @@ pub(crate) struct Window {
     db: Arc<Database>,
     index: QueryIndex,
 
-    start_id: StreamID,
-    end_id: StreamID,
+    stream_id_limit: StreamID,
     size: usize,
     attached: bool,
 
-    start_id_for_reattach: Option<StreamID>,
+    limit_for_reattach: Option<StreamID>,
+
+    start_idx: usize,
+    end_idx: usize,
 
     latest_id_chan: Throttle<watch::Receiver<StreamID>>,
     params_rx: mpsc::Receiver<WindowParameters>,
@@ -42,21 +44,21 @@ impl Window {
         assert!(*index == QueryIndex::All, "TODOtodoTODO");
         let (params_tx, params_rx) = mpsc::channel(1);
         let mut latest_id_chan = db.stream_notification_rx.clone();
-        let start_id = latest_id_chan.recv().await.unwrap();
-        (
-            Window {
-                db,
-                size: 0,
-                start_id,
-                end_id: start_id,
-                attached: true,
-                start_id_for_reattach: None,
-                latest_id_chan: throttle(std::time::Duration::MILLISECOND * 250, latest_id_chan),
-                params_rx,
-                index: *index,
-            },
-            Arc::new(WindowHandle(params_tx)),
-        )
+        let stream_id_limit = latest_id_chan.recv().await.unwrap();
+        let mut window = Window {
+            db,
+            size: 0,
+            stream_id_limit,
+            start_idx: 0,
+            end_idx: 0,
+            attached: true,
+            limit_for_reattach: None,
+            latest_id_chan: throttle(std::time::Duration::MILLISECOND * 250, latest_id_chan),
+            params_rx,
+            index: *index,
+        };
+        window.fixup_start_end_idx().await;
+        (window, Arc::new(WindowHandle(params_tx)))
     }
 
     pub(crate) async fn set_attached(&mut self, attached: bool) -> Option<WindowUpdate> {
@@ -68,7 +70,7 @@ impl Window {
             None
         } else {
             self.attached = true;
-            if let Some(start_id) = self.start_id_for_reattach.take() {
+            if let Some(start_id) = self.limit_for_reattach.take() {
                 self.new_id(start_id).await
             } else {
                 None
@@ -117,23 +119,25 @@ impl Window {
         if size > self.size {
             let old_size = self.size;
             self.size = size;
+            self.end_idx = self.start_idx.checked_sub(self.size).unwrap_or(0);
             let mut extended = Vec::new();
             match self.index {
                 QueryIndex::All => {
                     let streams = self.db.streams.read().await;
-                    if self.start_id.idx() - self.end_id.idx() < self.size {
-                        self.end_id = if self.end_id.idx() <= self.size {
-                            StreamID::new(0)
-                        } else {
-                            StreamID::new(self.start_id.idx() - self.size)
-                        }
-                    }
-                    let slice = &streams[self.end_id.idx()..self.start_id.idx()];
+                    let slice = &streams[self.end_idx..self.start_idx];
                     for elem in &slice[..(size - old_size)] {
                         extended.push(elem.as_lightweight());
                     }
                 }
-                _ => todo!(),
+                _ => {
+                    let streams = self.db.streams.read().await;
+                    self.with_streamid_slice(|stream_ids| {
+                        for elem in &stream_ids[..(size - old_size)] {
+                            extended.push(streams[elem.idx()].as_lightweight());
+                        }
+                    })
+                    .await;
+                }
             }
             return Some(WindowUpdate {
                 extended,
@@ -142,30 +146,92 @@ impl Window {
         }
         None
     }
+
+    pub(crate) fn binsearch(slice: &[StreamID], id: StreamID) -> usize {
+        for (i, elem) in slice.iter().rev().enumerate().take(16) {
+            if elem.idx() <= id.idx() {
+                return slice.len() - i - 1;
+            }
+        }
+
+        let mut l = 0;
+        let mut r = slice.len() - 1;
+        while l <= r {
+            let m = (l + r) / 2;
+            if slice[m].idx() > id.idx() {
+                r = m + 1;
+            } else if slice[m].idx() < id.idx() {
+                l = m - 1;
+            } else {
+                return m;
+            }
+        }
+        l
+    }
+
     pub(crate) async fn new_id(&mut self, id: StreamID) -> Option<WindowUpdate> {
         if !self.attached {
-            self.start_id_for_reattach = Some(id);
+            self.limit_for_reattach = Some(id);
             return None;
         }
 
-        let prev_start = std::mem::replace(&mut self.start_id, id);
+        self.stream_id_limit = id;
         let mut new = Vec::new();
         match self.index {
             QueryIndex::All => {
-                let new_cnt = self.start_id.idx() - prev_start.idx();
-                self.end_id = StreamID::new(self.end_id.idx() + new_cnt);
+                let new_cnt = id.idx() - self.start_idx;
+                self.start_idx = id.idx();
+                self.end_idx = self.end_idx + new_cnt;
                 let cnt = new_cnt.min(self.size);
                 let streams = self.db.streams.read().await;
-                for elem in &streams[self.start_id.idx() - cnt..][..cnt] {
+                for elem in &streams[self.start_idx - cnt..][..cnt] {
                     new.push(elem.as_lightweight());
                 }
             }
-            _ => todo!(),
+            _ => {
+                let prev_idx = self.start_idx;
+                let db = self.db.clone();
+                let streams = db.streams.read().await;
+                let (start_idx, end_idx) = self
+                    .with_streamid_slice(|stream_ids| {
+                        let start_idx = Self::binsearch(stream_ids, id);
+                        let end_idx = self.start_idx.checked_sub(self.size).unwrap_or(0);
+                        let send_cnt = start_idx - prev_idx;
+                        for elem in &stream_ids[end_idx..start_idx][..send_cnt] {
+                            new.push(streams[elem.idx()].as_lightweight());
+                        }
+                        (start_idx, end_idx)
+                    })
+                    .await;
+                self.start_idx = start_idx;
+                self.end_idx = end_idx;
+            }
         }
         Some(WindowUpdate {
             new,
             ..WindowUpdate::default()
         })
+    }
+
+    pub(crate) async fn fixup_start_end_idx(&mut self) {
+        if let QueryIndex::All = self.index {
+            self.start_idx = self.stream_id_limit.idx();
+            self.end_idx = self.start_idx;
+            return;
+        }
+        let stream_id = self.stream_id_limit;
+        let (idx, _sid) = self
+            .with_streamid_slice(|stream_ids| {
+                for (i, elem) in stream_ids.iter().rev().enumerate() {
+                    if elem.idx() <= stream_id.idx() {
+                        return (stream_ids.len() - i - 1, *elem);
+                    }
+                }
+                (0usize, StreamID::new(0))
+            })
+            .await;
+        self.start_idx = idx;
+        self.end_idx = idx;
     }
 
     pub(crate) async fn stream_results_to(
